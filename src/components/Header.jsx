a039c97102e61f756883173c204b76a5aa286446
reacton <aliyasin.emir16@gmail.com>
2024-02-17 19:08:26 +0300

    I use useRef method insead of onChange

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <h1>THE <em>ALMOST</em> FINAL COUNTDOWN</h1>
+      <p>Stop the timer once you estimate that time is (almost) up</p>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/Player.jsx
@@ -0,0 +1,50 @@
+"use client"
+
+import { useRef, useState } from "react";
+
+// import { useState } from "react";
+
+
+export default function Player() {
+
+// Show text with onChange() method //
+
+//  const [playerName, setPlayerName] = useState("Player")
+//  const [isSubmitted, setIsSubmitted] = useState(false)
+
+//   const handleChange = (e) => {
+//     setIsSubmitted(false)
+//     setPlayerName(e.target.value)
+//   }
+
+//   const handleClick = () => {
+//     setIsSubmitted(true)
+//   }
+
+//  onChange={handleChange}
+
+// value={playerName}
+
+// isSubmitted ? `Player ${playerName}` : "Player"
+
+
+// Show text with useRef() method //
+
+  const [editableName, setEditableName] = useState("Player")
+
+  const handleClick = () => {
+    setEditableName(editableNameRef.current.value)
+  }
+
+  const editableNameRef = useRef()
+
+  return (
+    <section id="player">
+      <h2 className="text-3xl mb-6">Welcome {editableName}</h2>
+      <p>
+        <input ref={editableNameRef} type="text" />
+        <button onClick={handleClick}>Set Name</button>
+      </p>
+    </section>
+  );
+}
